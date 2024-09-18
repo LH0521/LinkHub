6e97c3fbb1e2f55ffbabf2e0fd779f489ed10dcd
Lucas Hansen <lucashansen0521@gmail.com>
2024-09-17 20:07:32 -0400

    Forgot to add testing links :P

---
+++ Assets/Data/links.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        "name": "ExhibBoy",
+        "link": "PvblicBoy",
+        "icon": "1819821904653365248/LCaWV3wa_400x400.jpg",
+        "info": {
+            "preview": ["https://pbs.twimg.com/media/GXqWMwUXgAApYY_?format=png", "https://pbs.twimg.com/media/GPRinQbXUAAOZad?format=png"],
+            "kinks": ["Exhibition"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "twitter"
+        }
+    },
+    {
+        "name": "Jason",
+        "link": "jason1",
+        "icon": "1819821904653365248/LCaWV3wa_400x400.jpg",
+        "info": {
+            "preview": ["https://pbs.twimg.com/media/GXqWMwUXgAApYY_?format=png"],
+            "kinks": ["Feet"],
+            "sports": [],
+            "experience": "Professional",
+            "people": "Solo",
+            "activity": "Active",
+            "sexuality": "Gay",
+            "body": "Twink",
+            "race": "Latino",
+            "source": "reddit"
+        }
+    }
+];
